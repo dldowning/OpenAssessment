8fa4a8e557647656519ae5b88e6d9cc90cc2e53d
dldowning <90722567+dldowning@users.noreply.github.com>
2022-06-14 09:44:21 -0500

    new db structure

--- questDDL.sql
+++ questDDL.sql
@@ -2,17 +2,60 @@ create database Assessment;
 
 use Assessment;
 
-create table quests
-(
-q_id int Primary key,
-Label varchar(25),
-quest varchar(120),
-op_1 varchar(25),
-op_2 varchar(25),
-op_3 varchar(25),
-op_4 varchar(25),
-ans varchar(25)
+CREATE TABLE User_Table (
+  user_id int NOT NULL AUTO_INCREMENT,
+  student_id int,
+  First_Name varchar(255),
+  Last_Name varchar(255) NOT NULL,
+  Email varchar(255),
+  Registered_On timestamp,
+  Degree varchar(255),
+  Country varchar(255),
+  PRIMARY KEY (user_id),
 );
 
-select * from quests;
+CREATE TABLE Questions_Table (
+  quest_id int NOT NULL AUTO_INCREMENT,
+  topic varchar(255),
+  difficulty int,
+  Created_on timestamp,
+  Content varchar(255),
+  PRIMARY KEY (quest_id),
+);
+
+CREATE TABLE Course_Table (
+  course_id int NOT NULL AUTO_INCREMENT,
+  course_number int,
+  topic varchar(255),
+  quantity int,
+  summary varchar(255),
+  PRIMARY KEY (course_id),
+  FOREIGN KEY (topic) REFERENCES Questions_Table(topic),
+);
+
+CREATE TABLE Options_Table (
+  options_id int NOT NULL AUTO_INCREMENT,
+  quest_id int,
+  is_correct boolean,
+  created_on date,
+  Content varchar(255),
+  PRIMARY KEY (options_id),
+  FOREIGN KEY (quest_id) REFERENCES Questions_Table(quest_id),
+);
+
+CREATE TABLE Results Table (
+  results_id int NOT NULL AUTO_INCREMENT,
+  quest_id int,
+  options_id int,
+  course_id int,
+  user_id int,
+  attempt_id int,
+  taken_on timestamp,
+  expiration datetime,
+  PRIMARY KEY (results_id),
+  FOREIGN KEY (user_id) REFERENCES User_Table(user_id),
+  FOREIGN KEY (options_id) REFERENCES Options_Table(options_id),
+  FOREIGN KEY (quest_id) REFERENCES Questions_Table(quest_id),
+  FOREIGN KEY (course_id) REFERENCES Course_Table(course_id),
+);
 
